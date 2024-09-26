5bb7593fce977a0d08443a4b0bfb4607ae088f18
jeromecoffin <jerome.devops@gmail.com>
2024-09-26 16:22:38 +0700

    projects

--- landing.py
+++ landing.py
@@ -25,17 +25,15 @@ st.markdown("""
 
 st.markdown("""
 <div class="text-center py-8">
-  <h1 class="text-2xl font-bold mb-4"> Let's create an innovative tech solution for your fashion business.</h1>
+  <h1 class="text-2xl font-bold mb-4">Empower Your Business with Custom Cloud Solutions</h1>
 </div>
 """, unsafe_allow_html=True)
 
 st.markdown("""
 <div class="text-center py-8">
-  <p class="text-lg max-w-4xl mx-auto">I develop innovative tech solutions for the fashion and luxury industries, 
-            empowering brands, influencers, sourcing agents... and all stakeholders.
-            My projects include personalized data visualization tools, scrapers and custome Web Application.
-            I also share my passion for fashion on my <a href="https://www.instagram.com/gijon_coffin">public Instagram account</a>.</p>
-<h1 class="text-1xl font-bold mb-6"> Contact me : jerome.devops@gmail.com</h1>
+  <p class="text-lg max-w-4xl mx-auto">I develop tailored tech solutions for businesses in any industry, delivering custom web apps, data visualization tools, and web scrapers that drive efficiency and insight for all types of stakeholders.
+            <br/>I also share my passion for fashion on my <a href="https://www.instagram.com/gijon_coffin">public Instagram account</a>. Don't be surprised, I might be the only tech-guy with a tie.</p>
+<h1 class="text-1xl font-bold mb-6"> Contact : jerome.devops@gmail.com</h1>
 </div>
 """, unsafe_allow_html=True)
 
@@ -65,29 +63,40 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
-# Title for the whole section
-st.markdown('<h2 class="text-3xl font-semibold text-center">My Projects</h2>', unsafe_allow_html=True)
+st.markdown(""" <div class="text-center py-8">
+  <div class="max-w-7xl mx-auto px-4">
+    <h2 class="text-3xl font-semibold text-center">My Projects</h2>
+    <a href="https://github.com/jeromecoffin" class="text-indigo-600 hover:underline text-xl">Github for source code</a>
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
+  
+  <!-- Project 1 -->
+  <div class="bg-white shadow-lg rounded-lg p-6">
+    <h3 class="text-xl font-semibold mb-4">Avanta-Sourcing : RFI Generator</h3>
+    <p class="text-gray-600 mb-4">Automate and streamline the Request for Information process for sourcing agents.</p>
+    <a href="https://www.avanta-sourcing.com" class="text-indigo-600 hover:underline">Learn More</a>
+  </div>
 
-# List of project titles
-projectTitle = ["RFI Generator for Sourcing Agents", "Luxury Handbag Price Tracker for Investors", "Influencer Campaign Management Tool"]
-projectDescription = ["Automate and streamline the Request for Information process for sourcing agents.", "A real-time price tracking tool for luxury handbags, designed for collectors and investors.", "An all-in-one platform for online brands to manage and optimize influencer marketing campaigns."]
-projectLink = ["https://github.com/jeromecoffin/sourcing", "https://github.com/jeromecoffin/scraper", "https://github.com/jeromecoffin/instafollower"]
-# Create a container for the project cards
-container = st.container()
+  <!-- Project 2 -->
+  <div class="bg-white shadow-lg rounded-lg p-6">
+    <h3 class="text-xl font-semibold mb-4">Luxury Handbag Price Tracker</h3>
+    <p class="text-gray-600 mb-4">A real-time price tracking tool for luxury handbags, designed for collectors and investors.</p>
+    <a href="https://github.com/jeromecoffin/scraper" class="text-indigo-600 hover:underline">Learn More</a>
+  </div>
 
-# Create a 3-column layout for the project cards
-row1 = container.columns(3)
+  <!-- Project 3 -->
+  <div class="bg-white shadow-lg rounded-lg p-6">
+    <h3 class="text-xl font-semibold mb-4">Influencer Campaign Management</h3>
+    <p class="text-gray-600 mb-4">An all-in-one platform for online brands to manage and optimize influencer marketing campaigns.</p>
+    <a href="https://github.com/jeromecoffin/instafollower" class="text-indigo-600 hover:underline">Learn More</a>
+  </div>
 
-# Display each project title in a styled "card"
-for idx, col in enumerate(row1):
-    with col:
-        st.markdown(f"""
-        <div class="card">
-            <div class="title">{projectTitle[idx]}</div>
-            <p>{projectDescription[idx]}</p>
-            <a href="{projectLink[idx]}" class="text-indigo-600 hover:underline text-xl">Learn More</a>
-        </div>
-        """, unsafe_allow_html=True)
+</div>
+  </div>
+            </div>""", unsafe_allow_html=True)
+
+
+st.markdown(f"""<br>""", unsafe_allow_html=True)
+st.divider()
 
 # Medium Section
 st.markdown("""
@@ -106,7 +115,7 @@ st.markdown("""
 
   <!-- Medium 2 -->
   <div class="bg-white shadow-lg rounded-lg p-6">
-    <h3 class="text-xl font-semibold mb-4">Streamlit on HTTPS for beginners</h3>
+    <h3 class="text-xl font-semibold mb-4">Streamlit on HTTPS</h3>
     <p class="text-gray-600 mb-4">Here is a quick, up-to-date explanation of how to set up Streamlit on HTTPS behind Nginx.</p>
     <a href="https://medium.com/@jerome.devops/finally-streamlit-on-https-for-beginners-4543d7793005" class="text-indigo-600 hover:underline">Learn More</a>
   </div>
@@ -122,6 +131,8 @@ st.markdown("""
   </div>
             </div>
 """, unsafe_allow_html=True)
+
+st.divider()
 
 # Instagram Section
 st.markdown("""
